4c364b6f1b24855d93b296851fa2bebd19328e0b
Analu Lisboa <analuizalisboa@gmail.com>
2022-10-07 20:00:10 -0300

    starts ios all workspace and inner projects

---
+++ Pods/Target Support Files/Pods-ios-apps-ios-appsUITests/Pods-ios-apps-ios-appsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_apps_ios_appsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_apps_ios_appsUITests
+@end
---
+++ ios-apps/libNetworking/libNetworking/libNetworking.h
@@ -0,0 +1,18 @@
+//
+//  libNetworking.h
+//  libNetworking
+//
+//  Created by Analu Lisboa on 07/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libNetworking.
+FOUNDATION_EXPORT double libNetworkingVersionNumber;
+
+//! Project version string for libNetworking.
+FOUNDATION_EXPORT const unsigned char libNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libNetworking/PublicHeader.h>
+
+
