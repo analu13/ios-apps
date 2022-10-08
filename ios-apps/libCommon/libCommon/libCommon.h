67bde68c10902692f2183887c51feb54e1665e58
Analu Lisboa <analuizalisboa@gmail.com>
2022-10-07 21:05:47 -0300

    [main] adds libCommon framework

---
+++ ios-apps/libCommon/libCommon/libCommon.h
@@ -0,0 +1,18 @@
+//
+//  libCommon.h
+//  libCommon
+//
+//  Created by Analu Lisboa on 07/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libCommon.
+FOUNDATION_EXPORT double libCommonVersionNumber;
+
+//! Project version string for libCommon.
+FOUNDATION_EXPORT const unsigned char libCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libCommon/PublicHeader.h>
+
+
